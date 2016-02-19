1b961a7e551b0c39dcd0fae9254560d6cc3868fb
apv1313 <apv1313@users.noreply.github.com>
2016-02-19 21:24:57 +0100

    Remove own comments

--- src/com/sbezboro/standardgroups/tasks/PowerRestorationTask.java
+++ src/com/sbezboro/standardgroups/tasks/PowerRestorationTask.java
@@ -18,9 +18,6 @@ public class PowerRestorationTask extends BaseTask {
 
 	@Override
 	public void run() {
-		// subPlugin.getLogger().info("Running PowerRestorationTask");
-		// -> Log spam? Once per minute
-
 		GroupManager groupManager = subPlugin.getGroupManager();
 		
 		float restoreAmount = StandardGroups.getPlugin().getGroupPowerGrowth();
@@ -28,8 +25,8 @@ public class PowerRestorationTask extends BaseTask {
 		for (Group group : groupManager.getGroups()) {
 			float numMembers = group.getPlayerCount();
 			float numNonAltMembers = group.getNonAltPlayerCount();
-			float onlineModifier = (float)(group.getNonAltOnlineCount()) / numMembers; // sic: Groups with alts cannot get full regen
-			float memberModifier = 3.0f - 6.0f/(numNonAltMembers+2.0f); // 0 -> 0, 1 -> 1, 2 -> 1.5, 3 -> 1.8, 5 -> 2.14, 10 -> 2.5, inf -> 3
+			float onlineModifier = (float)(group.getNonAltOnlineCount()) / numMembers;
+			float memberModifier = 3.0f - 6.0f/(numNonAltMembers+2.0f);
 			float altPenalty = Math.max((numMembers-numNonAltMembers)/numMembers - 0.25f, 0.0f);
 			group.addPower(restoreAmount * (memberModifier * onlineModifier - altPenalty));
 		}
