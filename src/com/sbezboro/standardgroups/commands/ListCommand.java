788ac363b683186d46be1e58ecae37f663218e74
apv1313 <apv1313@users.noreply.github.com>
2016-05-02 22:01:01 +0200

    Add option to order group list by power/maxPower

--- src/com/sbezboro/standardgroups/commands/ListCommand.java
+++ src/com/sbezboro/standardgroups/commands/ListCommand.java
@@ -9,6 +9,7 @@ import com.sbezboro.standardplugin.model.StandardPlayer;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
+import java.util.Comparator;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,30 +19,52 @@ public class ListCommand extends SubCommand {
 		super(plugin, command, "list");
 
 		addHelp(ChatColor.YELLOW + "/g list [page]" + ChatColor.RESET + " - show all active groups");
+		addHelp(ChatColor.YELLOW + "/g list p [page]" + ChatColor.RESET + " - show groups ordered by current power");
+		addHelp(ChatColor.YELLOW + "/g list mp [page]" + ChatColor.RESET + " - show groups ordered by maximum power");
 	}
 
 	@Override
 	public boolean handle(CommandSender sender, String[] args) {
 		GroupManager groupManager = StandardGroups.getPlugin().getGroupManager();
+		
+		int page;
+		Comparator<Group> comparator;
 
-		if (args.length > 1) {
-			command.showUsageInfo(sender);
-			return false;
-		} else if (args.length == 1) {
-			int page;
-
+		if (args.length == 2) {
+			if (args[0].equals("p")) {
+				comparator = new GroupManager.PowerComparator();
+			} else if (args[0].equals("mp")) {
+				comparator = new GroupManager.MaxPowerComparator();
+			} else {
+				comparator = null;
+			}
+			
 			try {
-				page = Integer.parseInt(args[0]);
+				page = Integer.parseInt(args[1]);
 			} catch (NumberFormatException e) {
 				page = 1;
 			}
-
-			groupManager.groupList(sender, page);
+		} else if (args.length == 1) {
+			if (args[0].equals("p")) {
+				comparator = new GroupManager.PowerComparator();
+				page = 1;
+			} else if (args[0].equals("mp")) {
+				comparator = new GroupManager.MaxPowerComparator();
+				page = 1;
+			} else {
+				comparator = null;
+				try {
+					page = Integer.parseInt(args[0]);
+				} catch (NumberFormatException e) {
+					page = 1;
+				}
+			}
 		} else {
-			groupManager.groupList(sender, 1);
+			comparator = null;
+			page = 1;
 		}
-		
 
+		groupManager.groupList(sender, page, comparator);
 		
 		return true;
 	}
