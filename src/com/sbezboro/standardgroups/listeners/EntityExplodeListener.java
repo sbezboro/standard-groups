32760c05563d1b653a2c83f39505b591f49e03ed
apv1313 <apv1313@users.noreply.github.com>
2016-05-02 22:43:55 +0200

    Block TNT (/minecarts) under certain circumstances

--- src/com/sbezboro/standardgroups/listeners/EntityExplodeListener.java
+++ src/com/sbezboro/standardgroups/listeners/EntityExplodeListener.java
@@ -6,12 +6,14 @@ import com.sbezboro.standardgroups.model.Group;
 import com.sbezboro.standardplugin.StandardPlugin;
 import com.sbezboro.standardplugin.SubPluginEventListener;
 import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EntityExplodeListener extends SubPluginEventListener<StandardGroups> implements Listener {
 
@@ -22,6 +24,8 @@ public class EntityExplodeListener extends SubPluginEventListener<StandardGroups
 	@EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
 	public void onEntityExplode(final EntityExplodeEvent event) {
 		GroupManager groupManager = subPlugin.getGroupManager();
+		
+		List<Group> affectedGroups = new ArrayList<Group>();
 
 		for (Block block : new ArrayList<Block>(event.blockList())) {
 			Group group = groupManager.getGroupByLocation(block.getLocation());
@@ -32,11 +36,35 @@ public class EntityExplodeListener extends SubPluginEventListener<StandardGroups
 					return;
 				}
 				
+				// Since TNT can now be placed in raids, disable TNT minecarts on claimed land
+				if (event.getEntity().getType() == EntityType.MINECART_TNT) {
+					event.setCancelled(true);
+					return;
+				}
+				
 				if (!groupManager.getLocksAffectedByBlock(block.getLocation()).isEmpty()) {
 					event.blockList().remove(block);
 				}
+				
+				if (!affectedGroups.contains(group)) {
+					affectedGroups.add(group);
+				}
 			}
 		}
+
+		// Only allow as many TNT explosions to happen as TNT blocks have been placed inside groups' land
+		if (event.getEntity().getType() == EntityType.PRIMED_TNT && !affectedGroups.isEmpty()) {
+			for (Group group : affectedGroups) {
+				if (group.getAllowedTnt() <= 0) {
+					event.setCancelled(true);
+					return;
+				}
+			}
+			for (Group group : affectedGroups) {
+				group.decrementAllowedTnt();
+			}
+		}
+		
 	}
 
 }
