31873434ac944022a1131fa4ef0b779b64c5b674
Tincon1 <brendan.gb@hotmail.com>
2016-06-10 00:49:08 -0500

    Update GroupManager.java
    Added skulls to protected blocks

--- src/com/sbezboro/standardgroups/managers/GroupManager.java
+++ src/com/sbezboro/standardgroups/managers/GroupManager.java
@@ -82,6 +82,7 @@ public class GroupManager extends BaseManager {
 		add(Material.BED_BLOCK);
 		add(Material.LEVER);
 		add(Material.STONE_BUTTON);
+                add(Material.SKULL);
 	}};
 
 	@SuppressWarnings("serial")
