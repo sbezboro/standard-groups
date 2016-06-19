34e6420fae5b25fc0653341f64dab77b055b0328
apv1313 <apv1313@users.noreply.github.com>
2016-06-19 19:07:40 +0200

    New /g gm (group message) command

---
+++ src/com/sbezboro/standardgroups/commands/GroupMessageCommand.java
@@ -0,0 +1,39 @@
+package com.sbezboro.standardgroups.commands;
+
+import com.sbezboro.standardgroups.StandardGroups;
+import com.sbezboro.standardgroups.managers.GroupManager;
+import com.sbezboro.standardplugin.StandardPlugin;
+import com.sbezboro.standardplugin.commands.BaseCommand;
+import com.sbezboro.standardplugin.commands.SubCommand;
+import com.sbezboro.standardplugin.model.StandardPlayer;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+
+public class GroupMessageCommand extends SubCommand {
+
+	public GroupMessageCommand(StandardPlugin plugin, BaseCommand command) {
+		super(plugin, command, "gm");
+
+		addHelp(ChatColor.YELLOW + "/g gm <message>/off" + ChatColor.RESET + " - set a message for your group members to see upon login");
+	}
+
+	@Override
+	public boolean handle(CommandSender sender, String[] args) {
+		StandardPlayer player = plugin.getStandardPlayer(sender);
+		
+		if (player == null) {
+			command.showPlayerOnlyMessage(sender);
+			return false;
+		}
+		
+		GroupManager groupManager = StandardGroups.getPlugin().getGroupManager();
+		if (args.length == 0) {
+			groupManager.disableGroupMessage(player);
+		} else {
+			groupManager.setGroupMessage(player, String.join(" ", args));
+		}
+		
+		return true;
+	}
+	
+}
