37c68fd96e4d62665dd270e3824a075b38b712d9
tincon <tincon1mc@gmail.com>
2019-02-11 05:43:14 -0600

    Added a group name filter

--- src/com/sbezboro/standardgroups/commands/CreateCommand.java
+++ src/com/sbezboro/standardgroups/commands/CreateCommand.java
@@ -1,52 +1,54 @@
 package com.sbezboro.standardgroups.commands;
 
-import org.bukkit.ChatColor;
-import org.bukkit.command.CommandSender;
-
 import com.sbezboro.standardgroups.StandardGroups;
 import com.sbezboro.standardgroups.managers.GroupManager;
 import com.sbezboro.standardplugin.StandardPlugin;
 import com.sbezboro.standardplugin.commands.BaseCommand;
 import com.sbezboro.standardplugin.commands.SubCommand;
 import com.sbezboro.standardplugin.model.StandardPlayer;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
 
 public class CreateCommand extends SubCommand {
 
-	public CreateCommand(StandardPlugin plugin, BaseCommand command) {
-		super(plugin, command, "create");
+    public CreateCommand(StandardPlugin plugin, BaseCommand command) {
+        super(plugin, command, "create");
 
-		addHelp(ChatColor.YELLOW + "/g create <name>" + ChatColor.RESET + " - create a group");
-	}
+        addHelp(ChatColor.YELLOW + "/g create <name>" + ChatColor.RESET + " - create a group");
+    }
 
-	@Override
-	public boolean handle(CommandSender sender, String[] args) {
-		StandardPlayer player = plugin.getStandardPlayer(sender);
-		
-		if (player == null) {
-			command.showPlayerOnlyMessage(sender);
-			return false;
-		}
-		
-		GroupManager groupManager = StandardGroups.getPlugin().getGroupManager();
-		
-		if (groupManager.hasCommandCooldown(new String(player.getUuidString()), true)) {
-			groupManager.enableCommandCooldown(new String(player.getUuidString()));
-			return false;
-		}
-		
-		if (args.length != 1) {
-			sender.sendMessage("You must provide a name for your group.");
-			return false;
-		}
-		
-		groupManager.createGroup(player, args[0]);
-		
-		groupManager.enableCommandCooldown(new String(player.getUuidString()));
-		
-		return true;
-	}
+    @Override
+    public boolean handle(CommandSender sender, String[] args) {
+        StandardPlayer player = plugin.getStandardPlayer(sender);
+
+        if (player == null) {
+            command.showPlayerOnlyMessage(sender);
+            return false;
+        }
+
+        GroupManager groupManager = StandardGroups.getPlugin().getGroupManager();
+
+        if (groupManager.hasCommandCooldown(new String(player.getUuidString()), true)) {
+            groupManager.enableCommandCooldown(new String(player.getUuidString()));
+            return false;
+        }
+
+        if (args.length != 1) {
+            sender.sendMessage("You must provide a name for your group.");
+            return false;
+        }
+        for (String str : plugin.getConfig().getStringList("disabled-group-names")) {
+            if (args[0].contains(str) || (args[0].equalsIgnoreCase(str))) {
+                player.sendMessage("You cannot name your Group that!");
+                return false;
+            }
+        }
+
+        groupManager.createGroup(player, args[0]);
+
+        groupManager.enableCommandCooldown(new String(player.getUuidString()));
+
+        return true;
+    }
 
 }
