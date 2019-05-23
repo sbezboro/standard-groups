6f1b74bf650ef5601bec8b3298aab5e1647299a5
sbezboro <sergei.b.ru@gmail.com>
2019-05-23 00:04:03 -0400

    update to 1.14

--- src/com/sbezboro/standardgroups/managers/GroupManager.java
+++ src/com/sbezboro/standardgroups/managers/GroupManager.java
@@ -24,7 +24,7 @@ import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.type.Bed;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_13_R2.block.impl.CraftTrapdoor;
+import org.bukkit.craftbukkit.v1_14_R1.block.impl.CraftTrapdoor;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 
