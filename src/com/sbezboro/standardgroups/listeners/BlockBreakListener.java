b13fc6c485738542896131770a5d0e97aad838c2
sbezboro <sergei.b.ru@gmail.com>
2019-12-25 11:45:27 -0500

    update to 1.15

--- src/com/sbezboro/standardgroups/listeners/BlockBreakListener.java
+++ src/com/sbezboro/standardgroups/listeners/BlockBreakListener.java
@@ -8,7 +8,7 @@ import com.sbezboro.standardplugin.StandardPlugin;
 import com.sbezboro.standardplugin.SubPluginEventListener;
 import com.sbezboro.standardplugin.model.StandardPlayer;
 
-import net.minecraft.server.v1_14_R1.Enchantments;
+import net.minecraft.server.v1_15_R1.Enchantments;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
